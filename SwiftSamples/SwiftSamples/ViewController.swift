1bbb6dc07646931598b69566d07f90323cb4b314
Hariharan.S <Hariharan.S@thomsonreuters.com>
2019-06-12 18:31:33 +0530

    ProjectCreation

---
+++ SwiftSamples/SwiftSamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSamples
+//
+//  Created by Hari on 12/06/19.
+//  Copyright © 2019 Hari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
