2e0adaa98c25858fc0a6e09e2eef56ec8e4f0bf0
Hariharan.S <Hariharan.S@thomsonreuters.com>
2019-06-12 19:21:42 +0530

    Added Tableview

--- SwiftSamples/SwiftSamples/ViewController.swift
+++ SwiftSamples/SwiftSamples/ViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    weak var tableView  = UITableView ()
+    var StringArray = [Int]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for num in 1...10{
+            self.StringArray .append(num)
+        }
+
+        self.tableView?.delegate = self
+        self.tableView?.dataSource = self
     }
+    
+    //TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StringArray.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell  = self.tableView?.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
+        cell.textLabel?.text = "hari"
+        return cell
+    }
+    
+    
 
 
 }
