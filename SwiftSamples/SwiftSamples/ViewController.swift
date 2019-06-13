7e669d53ecbb38561376aef77b65c0be542ea546
Hariharan.S <Hariharan.S@thomsonreuters.com>
2019-06-13 21:03:45 +0530

    cell added

--- SwiftSamples/SwiftSamples/ViewController.swift
+++ SwiftSamples/SwiftSamples/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     var StringArray = [Int]()
-    
+     var name = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +21,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             self.StringArray .append(num)
         }
 
+        self.navigationItem.title = "Sampel Swift"
         self.tableView?.delegate = self
         self.tableView?.dataSource = self
+        
+        self.fetchFromDb()
     }
     //TableViewDataSource
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -30,17 +34,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return name.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell :HomeCellVC =  tableView.dequeueReusableCell(withIdentifier: "homeCellVC", for: indexPath) as! HomeCellVC
        // let cell : UITableViewCell  = self.tableView?.dequeueReusableCell(withIdentifier: "homeCellVC") as! HomeCellVC
         
-        cell.nameLbl?.text = "Hari On Nama shivaya!!!"
+        cell.nameLbl?.text = name[indexPath.row]
         return cell
     }
     
+    func fetchFromDb(){
+        guard  let appdelgate : AppDelegate = UIApplication.shared.delegate as! AppDelegate else {return}
+        let manageContext = appdelgate.persistentContainer.viewContext
+        
+        let fetchRequest  = NSFetchRequest<NSFetchRequestResult>(entityName: "Albums")
+       
+        do{
+            let albums = try!manageContext.fetch(fetchRequest)
+            for data in albums as! [NSManagedObject]{
+                name.append(data.value(forKey: "title") as! String) as! String
+            }
+            
+        }
+    }
     
 
 
